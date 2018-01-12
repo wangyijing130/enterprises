675f0dceeff10672d281f00a2d4e6ac43943918d
wangyijing130 <wangyijing130@qq.com>
2018-01-12 15:07:41 +0800

    轻应用模块完成
    轻应用模块完成

--- src/common/swiper.js
+++ src/common/swiper.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {
+    View, StyleSheet, Image,
+    Platform, Touchable,
+    TouchableOpacity,
+    TouchableNativeFeedback,
+} from 'react-native';
 import Swiper from 'react-native-swiper';
 import {THEME_LIGHT} from '../assets/css/color';
 export class SwiperBox extends Component {
@@ -17,27 +22,39 @@ export class SwiperBox extends Component {
         }, 0)
     }
 
+    openLink(item) {
+        if (item.link) {
+            this.props.navigation.navigate('MyView', {url: item.link, title: item.title ? item.title : '链接'});
+            return;
+        }
+    }
+
+    renderList(data) {
+        return data.map(item => this.renderItem(item));
+    }
+
+    renderItem(item) {
+        const Touchable = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+        return (
+            <Touchable key={item.id} onPress={() => this.openLink(item)}>
+                <View style={style.swiperItem}>
+                    <Image style={style.img} resizeMode='contain'
+                           source={{uri: item.img}}
+                    />
+                </View>
+            </Touchable>
+        );
+    }
+
+
     render() {
-        if (this.state.swiperShow) {
+        let data = this.props.data || [];
+        if (this.state.swiperShow && data.length) {
             return (
                 <View style={style.wrapper}>
                     <Swiper style={style.swiper} autoplay={true} autoplayTimeout={10} activeDotColor='#fff'
                             paginationStyle={{bottom: 1}}>
-                        <View style={style.swiperItem}>
-                            <Image style={style.img} resizeMode='contain'
-                                   source={{uri: 'https://b-ssl.duitang.com/uploads/item/201310/07/20131007112359_VYSfX.thumb.700_0.jpeg'}}
-                            />
-                        </View>
-                        <View style={style.swiperItem}>
-                            <Image style={style.img} resizeMode='contain'
-                                   source={{uri: 'https://b-ssl.duitang.com/uploads/item/201310/07/20131007112359_VYSfX.thumb.700_0.jpeg'}}
-                            />
-                        </View>
-                        <View style={style.swiperItem}>
-                            <Image style={style.img} resizeMode='contain'
-                                   source={{uri: 'https://b-ssl.duitang.com/uploads/item/201310/07/20131007112359_VYSfX.thumb.700_0.jpeg'}}
-                            />
-                        </View>
+                        {this.renderList(data)}
                     </Swiper>
                 </View>
             );
